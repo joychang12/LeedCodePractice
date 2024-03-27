88ff5f033dac3af11a686af18ab35e736b743de2
joy chang <joychang1266@gmail.com>
2024-03-27 22:58:55 +0800

    create 55. Jump Game

---
+++ Arrary/55. Jump Game.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n = 0;
+        for(int i = 0;i < nums.length;i++){
+            if(n < i){
+                return false;
+            }
+
+            n = Math.max(n, i + nums[i]);
+        }
+        return true;
+    }
+}
